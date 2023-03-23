0bf3d25142787c6aa564c33bfe6cd1ae8e3cd131
vladya905 <lubajkinvlad@gmail.com>
2023-03-23 23:57:33 +0200

    commit1

--- js/buy-now.js
+++ js/buy-now.js
@@ -1,11 +1,15 @@
-const refs = {
-    openMenuBtn: document.querySelector('[data-buy-open]'),
-    closeMenuBtn: document.querySelector('[data-buy-close]'),
-    menu: document.querySelector('[data-buy]'),
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-buynow-open]"),
+    closeMenuBtn: document.querySelector("[data-buynow-close]"),
+    menu: document.querySelector("[data-buynow]"),
   };
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
+
+  refs.openMenuBtn.addEventListener("click", togglemenu);
+  refs.closeMenuBtn.addEventListener("click", togglemenu);
+
+  function togglemenu() {
+    refs.menu.classList.toggle("is-hidden");
     document.body.classList.toggle('no-scroll');
   }
+})();
