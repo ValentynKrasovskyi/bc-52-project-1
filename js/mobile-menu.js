e38dcf0a1dceafdcb863d6377bfc3bad75a4591e
YPV0 <yuriisfw@gmail.com>
2023-03-24 20:58:11 +0200

    Smooth-scroll-added, ID-s added

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -9,3 +9,29 @@ menuOpenButton.addEventListener('click', () => {
 menuCloseButton.addEventListener('click', () => {
   menu.classList.add('is-hidden');
 });
+
+const smoothScrollLinks = document.querySelectorAll('.smooth-scroll');
+
+
+smoothScrollLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    
+    const targetId = link.getAttribute('href');
+
+    const targetSection = document.querySelector(targetId);
+
+    targetSection.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
