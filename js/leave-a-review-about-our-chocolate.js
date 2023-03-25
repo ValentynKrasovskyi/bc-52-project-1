2a4bfbe12069148f909335e180fcf2d751da8e1d
ValentynKrasovskyi <krasovskyi00@gmail.com>
2023-03-25 11:29:50 +0100

    modals

--- js/leave-a-review-about-our-chocolate.js
+++ js/leave-a-review-about-our-chocolate.js
@@ -1,8 +1,8 @@
 const refs = {
-    openMenuBtn: document.querySelector('[data-review-open]'),
-    closeMenuBtn: document.querySelector('[data-review-close]'),
-    menu: document.querySelector('[data-review]'),
-  };
+  openMenuBtn: document.querySelector('[data-review-open]'),
+  closeMenuBtn: document.querySelector('[data-review-close]'),
+  menu: document.querySelector('[data-review]'),
+};
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
   function toggleMenu() {
